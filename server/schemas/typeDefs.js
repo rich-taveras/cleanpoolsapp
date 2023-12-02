facebc19877bb283c404a9db35948dc5edabae44
rich-taveras <rtaveras@yahoo.com>
2023-12-02 16:16:19 -0500

    initial commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+input BookInput{
+  authors: [String]
+  description: String
+  bookId: String
+  image: String
+  link: String
+  title: String!
+}
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
